8755ab1be90ede946decd782ec8176e3f9e9e036
Amy Simpson <simpson.r.amy@gmail.com>
2020-07-17 19:54:45 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -20,6 +20,15 @@ function pressIt(){
   });
 }
 
+
+$(document).on('keydown', function(key) {
+  if(key.which == 83){
+      alert('s was pressed');
+  }
+});
+
+
+
 function getIt(){
   $('p').on('click', function(){
   alert("Hey!");
